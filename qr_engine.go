95e70eb085384c965c3fbc56724a3b2f3857b92e
vuong <vuongt2@aalto.fi>
2019-08-09 14:38:43 +0300

    qr zxing

--- qr_engine.go
+++ qr_engine.go
@@ -1,21 +1,24 @@
 package ocrworker
 import (
 	"bytes"
+	"strings"
 
-	"github.com/tuotoo/qrcode"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
 )
 
 type QREngine struct {
 }
 
 func (m QREngine) ProcessRequest(ocrRequest OcrRequest) (OcrResult, error) {
-	fi := bytes.NewReader(ocrRequest.ImgBytes)
-
-	qrmatrix, err := qrcode.Decode(fi)
+	bmp := bytes.NewReader(ocrRequest.ImgBytes)
+	qrReader := qrcode.NewQRCodeReader()
+	result, err := qrReader.Decode(bmp, nil)
 	if err != nil{
     	failOnError("Failed to qr-decode image: %v", err)
-	    return OcrResult{Text: ""}, nil
+	    return OcrResult{Text: string(err)}, nil
 	}
+	
 
-	return OcrResult{Text: qrmatrix.Content}, nil
+	return OcrResult{Text: result}, nil
 }
